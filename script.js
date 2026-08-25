623c27c24b38926a948c78981c72673a7f923be5
Akash2-2kumar <106278593+Akash2-2kumar@users.noreply.github.com>
2026-08-25 17:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let a=document.querySelector("#app");
+a.innerHTML="<p id="counter">0</p>";
+a.innerHTML+="<button id="increment">Increment</button>";
+let b=document.getElementById(increment);
+increment.addEventListener("click",()=>{
+	alert(counter.textContent);
+	counter.textContent=Number(counter.textContent)+1;
+})
