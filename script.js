84b60ad5d04a7d415ea8c8d8cff609786d994abd
Akash2-2kumar <106278593+Akash2-2kumar@users.noreply.github.com>
2026-08-25 17:24:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 let a=document.querySelector("#app");
 a.innerHTML="<p id="counter">0</p>";
-a.innerHTML+="<button id="increment">Increment</button>";
-let b=document.getElementById(increment);
-increment.addEventListener("click",()=>{
+a.innerHTML+="<button id="incrementBtn">Increment</button>";
+let b=document.getElementById(incrementBtn);
+let c=counter.textContent;
+incrementBtn.addEventListener("click",()=>{
 	alert(counter.textContent);
-	counter.textContent=Number(counter.textContent)+1;
+	counter.textContent=Number(c)+1;
 })
